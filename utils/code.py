e5a8742fbb80b7f4fe91105294c93b9b27631b90
bywdf <bywdf@163.com>
2025-07-02 08:26:26 +0800

    2025.7.2 验证码代码

--- utils/code.py
+++ utils/code.py
@@ -20,19 +20,65 @@ def check_code(width=120, height=30, char_length=5, font_size=28):
     draw = ImageDraw.Draw(img, mode='RGB')
 
     def rndChar():
+        """随机生成一个字母"""
         return chr(random.randint(65, 90))
+        # return str(random.randint(0, 9)) 随机数字
 
     def rndColor():
+        """随机生成RGB颜色"""
         return (random.randint(0, 255), random.randint(10, 255), random.randint(64, 255))
        
-    # 其他验证码生成逻辑...
+    # 写文字
     for i in range(char_length):
         char = rndChar()
         code.append(char)
         h = random.randint(0, 4)
         draw.text([i * width / char_length, h], char, font=font, fill=rndColor())
     
-    # 其他绘制干扰线、点等代码...
+     # 写干扰点
+    for i in range(40):
+        draw.point([random.randint(0, width), random.randint(0, height)], fill=rndColor())
+ 
+    # 写干扰圆圈
+    for i in range(40):
+        draw.point([random.randint(0, width), random.randint(0, height)], fill=rndColor())
+        x = random.randint(0, width)
+        y = random.randint(0, height)
+        draw.arc((x, y, x + 4, y + 4), 0, 90, fill=rndColor())
+
+     # 画干扰线
+    for i in range(5):
+        x1 = random.randint(0, width)
+        y1 = random.randint(0, height)
+        x2 = random.randint(0, width)
+        y2 = random.randint(0, height)
+ 
+        draw.line((x1, y1, x2, y2), fill=rndColor())
+
     
     img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
     return img, ''.join(code)
+
+
+if __name__ == '__main__':
+    # 1. 直接打开
+    # img,code = check_code()
+    # img.show()
+ 
+    # 2. 写入文件
+    # img,code = check_code()
+    # with open('code.png','wb') as f:
+    #     img.save(f,format='png')
+ 
+    # 3. 写入内存(Python3)
+    # from io import BytesIO
+    # stream = BytesIO()
+    # img.save(stream, 'png')
+    # stream.getvalue()
+ 
+    # 4. 写入内存（Python2）
+    # import StringIO
+    # stream = StringIO.StringIO()
+    # img.save(stream, 'png')
+    # stream.getvalue()
+    pass
