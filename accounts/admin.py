addfa63199bd8392418a9ac4b3b360a4505e522b
bywdf <bywdf@163.com>
2025-07-11 16:19:15 +0800

    给用户注册搜索框

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,4 +3,16 @@ from django.contrib import admin
 from .models import UserInfo
 # Register your models here.
 
-admin.site.register(UserInfo)
+@admin.register(UserInfo)
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ('username', 'name', 'department', 'subject', 'phone')
+    search_fields = (
+        'username',
+        'name',
+        'phone',
+        'idnumber',
+        'department__title',  # 如果 Department 有 name 字段
+        'subject__title',      # 如果 Subject 有 name 字段
+    )
+    list_filter = ('gender', 'department', 'subject', 'professional_title')
+    ordering = ('-date_joined',)
