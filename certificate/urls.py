c0c5416e48e37fee059e449afe468b6fab55d453
bywdf <bywdf@163.com>
2025-10-10 10:44:28 +0800

    2025.10.10 校级证url完善

--- certificate/urls.py
+++ certificate/urls.py
@@ -9,9 +9,10 @@ app_name = 'certificate'
 urlpatterns = [
     # 校级证书基础管理
     path('shool_certificate/list/', school_certificate.school_certificate_list, name='school_certificate_list'),
-    path('shool_certificate/add/', school_certificate.school_certificate_list, name='school_certificate_add'),
-    path('shool_certificate/<int:nid>/edit/', school_certificate.school_certificate_list, name='school_certificate_edit'),
-    path('shool_certificate/delete/', school_certificate.school_certificate_list, name='school_certificate_delete'),
+    path('shool_certificate/add/', school_certificate.school_certificate_add, name='school_certificate_add'),
+    path('shool_certificate/<int:nid>/edit/', school_certificate.school_certificate_edit, name='school_certificate_edit'),
+    path('shool_certificate/delete/', school_certificate.school_certificate_delete, name='school_certificate_delete'),
+    path('shool_certificate/multi/', school_certificate.school_certificate_multi, name='school_certificate_multi'),
     
     # 校外证书基础管理
     
