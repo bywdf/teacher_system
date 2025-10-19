d31a8d64aa86d9493f25f9d85b0b0010bcbc9c56
bywdf <bywdf@163.com>
2025-10-19 14:42:03 +0800

    2025.10.19 SECRET_KEY

--- teacher_system/settings.py
+++ teacher_system/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-93zous^+i-nvxszm-apwtmj9++o0ojomi(81yv&s#z@ka2%i3&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,8 +85,6 @@ WSGI_APPLICATION = 'teacher_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
-load_dotenv()
 
 # 数据库配置
 DATABASES = {
